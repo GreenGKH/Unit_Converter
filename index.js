538e9e117ba69951dc05e79fe78e3def53b49faa
GreenGKH <118840705+GreenGKH@users.noreply.github.com>
2025-02-17 23:44:15 +0100

    finishing latet

--- index.js
+++ index.js
@@ -3,3 +3,46 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const lenghtEl = document.getElementById("length-box")
+const volumeEl = document.getElementById("volume-box")
+const massEl = document.getElementById("mass-box")
+const inputEl = document.getElementById("input-el")
+const buttonEl = document.getElementById("button-el")
+
+// Initial state
+let inputVal = 0
+
+// Functions
+buttonEl.addEventListener("click", function () {
+    inputVal = inputEl.value
+    render()
+})
+
+// Meter | feet
+function convertMtoF(inputVal) {
+    return inputVal * 3.281
+}
+function convertFtoM(inputVal) {
+    return inputVal / 3.281
+}
+
+//Liter | gallon
+function convertLtoG(inputVal) {
+    return inputVal * 0.264
+}
+function convertGtoL(inputVal) {
+    return inputVal / 0.264
+}
+
+// Kilogram | pound
+function convertKtoP(inputVal) {
+    return inputVal * 2.204
+}
+function convertPtoK(inputVal) {
+    return inputVal / 2.204
+}
+
+// Rendering
+function render() {
+}
